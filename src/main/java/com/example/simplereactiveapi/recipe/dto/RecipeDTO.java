3609278c0207b2916023dc8883a2cb7c4f237de2
David <depch47@gmail.com>
2022-06-05 12:45:00 -0500

    ADD---Validation through annotations

--- src/main/java/com/example/simplereactiveapi/recipe/dto/RecipeDTO.java
+++ src/main/java/com/example/simplereactiveapi/recipe/dto/RecipeDTO.java
@@ -5,13 +5,15 @@ import lombok.Data;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class RecipeDTO {
     private String id;
-    @NotBlank
+    @NotBlank(message = "Name is required")
     private String name;
     @Min(value = 1, message = "Price must be greater than 0")
-    @Max(value = 500, message = "Price must not be greater than 500")
+    @Max(value = 500, message = "Price cannot be greater than 500")
+    @NotNull
     private Integer price;
 }
--- src/main/java/com/example/simplereactiveapi/recipe/usecases/PutRecipeUseCase.java
+++ src/main/java/com/example/simplereactiveapi/recipe/usecases/PutRecipeUseCase.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
+
 @Service
 @AllArgsConstructor
 @Validated
@@ -16,7 +18,7 @@ public class PutRecipeUseCase {
     private final RecipeRepository repository;
     private final RecipeMapper mapper;
 
-    public Mono<RecipeDTO> apply(RecipeDTO recipeDTO) {
+    public Mono<RecipeDTO> apply(@Valid RecipeDTO recipeDTO) {
         return repository.findById(recipeDTO.getId())
                 .switchIfEmpty(Mono.error(() -> new IllegalStateException("Recipe does not exist: " + recipeDTO.getId())))
                 .flatMap(recipe -> repository.save(mapper.toEntity(recipeDTO)))
