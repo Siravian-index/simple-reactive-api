5613d5a9e0f9e03e9a72107f9581b7466cfd9fe3
David <depch47@gmail.com>
2022-06-07 08:25:19 -0500

    Adding Swagger to the app. GET method is working

--- src/main/java/com/example/simplereactiveapi/recipe/routes/GetRecipesRoute.java
+++ src/main/java/com/example/simplereactiveapi/recipe/routes/GetRecipesRoute.java
@@ -1,11 +1,20 @@
 package com.example.simplereactiveapi.recipe.routes;
 
 import com.example.simplereactiveapi.recipe.dto.RecipeDTO;
+import com.example.simplereactiveapi.recipe.entity.Recipe;
 import com.example.simplereactiveapi.recipe.usecases.GetRecipesUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -16,11 +25,19 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class GetRecipesRoute {
     @Bean
+
+    @RouterOperation(path = "/v1/api/recipe/", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetRecipesUseCase.class, method = RequestMethod.GET, beanMethod = "apply",
+            operation = @Operation(operationId = "getRecipes", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation",
+                            content = @Content(schema = @Schema(implementation = Recipe.class)))}
+            ))
     public RouterFunction<ServerResponse> allRecipes(GetRecipesUseCase get) {
         return route(GET("/v1/api/recipe/"),
                 request -> ServerResponse.status(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromProducer(get.apply(), RecipeDTO.class))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromProducer(get.apply(), RecipeDTO.class))
         );
     }
 }
