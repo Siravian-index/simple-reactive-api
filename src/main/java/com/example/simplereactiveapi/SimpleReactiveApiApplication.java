70407e09a922e334f2a6dfb179daf6d772951e68
David <depch47@gmail.com>
2022-06-07 08:15:34 -0500

    Adding Swagger to the project, step 1 done

--- src/main/java/com/example/simplereactiveapi/SimpleReactiveApiApplication.java
+++ src/main/java/com/example/simplereactiveapi/SimpleReactiveApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.simplereactiveapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Simple Reactive API", version = "1.0.0", description = "Documentation APIs v1.0.0"))
 public class SimpleReactiveApiApplication {
 
     public static void main(String[] args) {
