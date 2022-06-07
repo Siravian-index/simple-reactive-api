947f755c27b04c55e68244f47aafed9fa4c2261e
David <depch47@gmail.com>
2022-06-07 08:35:12 -0500

    Adding Swagger to the app. GET ONE is working

--- src/main/java/com/example/simplereactiveapi/recipe/routes/GetOneRecipeRoute.java
+++ src/main/java/com/example/simplereactiveapi/recipe/routes/GetOneRecipeRoute.java
@@ -1,10 +1,19 @@
 package com.example.simplereactiveapi.recipe.routes;
 
+import com.example.simplereactiveapi.recipe.entity.Recipe;
 import com.example.simplereactiveapi.recipe.usecases.GetOneRecipeUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -14,6 +23,15 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class GetOneRecipeRoute {
     @Bean
+    @RouterOperation(path = "/v1/api/recipe/{id}", produces = {
+            MediaType.APPLICATION_JSON_VALUE},
+            beanClass = GetOneRecipeUseCase.class, method = RequestMethod.GET, beanMethod = "apply",
+            operation = @Operation(operationId = "getOneRecipe", responses = {
+                    @ApiResponse(responseCode = "200", description = "successful operation",
+                            content = @Content(schema = @Schema(implementation = Recipe.class))),
+                    @ApiResponse(responseCode = "404", description = "Invalid Recipe id supplied")},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id")}
+            ))
     public RouterFunction<ServerResponse> oneRecipe(GetOneRecipeUseCase getOne) {
         return route(GET("/v1/api/recipe/{id}"),
                 request -> getOne.apply(request.pathVariable("id"))
