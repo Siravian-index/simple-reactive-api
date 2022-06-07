36cb1bbd9088585d97601447af812b1ddfe07922
David <depch47@gmail.com>
2022-06-07 08:42:00 -0500

    Adding Swagger to the app. PUT is working

--- src/main/java/com/example/simplereactiveapi/recipe/routes/PostRecipeRoute.java
+++ src/main/java/com/example/simplereactiveapi/recipe/routes/PostRecipeRoute.java
@@ -45,5 +45,3 @@ public class PostRecipeRoute {
         );
     }
 }
-
-//requesBody swagger annotation
--- src/main/java/com/example/simplereactiveapi/recipe/routes/PutRecipeRoute.java
+++ src/main/java/com/example/simplereactiveapi/recipe/routes/PutRecipeRoute.java
@@ -1,11 +1,20 @@
 package com.example.simplereactiveapi.recipe.routes;
 
 import com.example.simplereactiveapi.recipe.dto.RecipeDTO;
+import com.example.simplereactiveapi.recipe.entity.Recipe;
+import com.example.simplereactiveapi.recipe.usecases.PostRecipeUseCase;
 import com.example.simplereactiveapi.recipe.usecases.PutRecipeUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -16,6 +25,13 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class PutRecipeRoute {
     @Bean
+    @RouterOperation(path = "/v1/api/recipe/", produces = {
+            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.PUT, beanClass = PutRecipeUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "updateRecipe", responses = {
+                    @ApiResponse(responseCode = "202", description = "successful operation", content = @Content(schema = @Schema(implementation = Recipe.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid Recipe details supplied")}
+                    , requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = Recipe.class)))
+            ))
     public RouterFunction<ServerResponse> putRecipe(PutRecipeUseCase update) {
         return route(PUT("/v1/api/recipe/").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(RecipeDTO.class)
